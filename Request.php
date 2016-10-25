c6271065779da34badee3f6be944789243a4283c
Kael Shipman <kael.shipman@gmail.com>
2016-10-25 11:09:00 -0500

    Improved URI handling in Request class

--- Request.php
+++ Request.php
@@ -3,6 +3,10 @@ namespace Skel;
 
 class Request extends \Symfony\Component\HttpFoundation\Request implements Interfaces\Request {
   public function getUri() {
-    return new Uri(parent::getRequestUri());
+    return new Uri($this->getFullRequestUrl());
+  }
+
+  public function getFullRequestUrl() {
+    return $this->getScheme().'://'.$this->getHost().':'.$this->getPort().$this->getRequestUri();
   }
 }
